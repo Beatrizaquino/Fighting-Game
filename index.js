14e89112d986047d5404006fd29a784ffb1c9d72
Beatriz Aquino <beatrizzoldyck07@gmail.com>
2023-07-23 20:38:07 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,61 @@
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
+
+canvas.width = 1024
+canvas.height = 576
+
+c.fillRect(0, 0, canvas.width, canvas.height)
+
+class Sprite {
+    //toda vez que criar um objeto da classe Spite vou chamar este função 
+    constructor({position, velocity}) {
+        this.position = position
+        this.velocity = velocity
+    }
+
+    draw() {
+        c.fillStyle = 'pink'
+        c.fillRect(this.position.x, this.position.y, 50, 150)
+    }
+
+    update() {
+        this.draw()
+        this.position.y += 10
+    }
+}
+
+const player = new Sprite({
+    position: {
+        x: 0,
+        y: 0
+    },
+    velocity: {
+        x: 0,
+        y: 0
+    }
+})
+
+
+const enemy = new Sprite({
+    position: {
+        x: 400,
+        y: 100
+    },
+    velocity: {
+        x: 0,
+        y: 0
+    }
+})
+
+
+function animate(){ 
+
+    window.requestAnimationFrame(animate)
+    c.fillRect(0, 0, canvas.width, canvas.height)
+    player.update()
+    enemy.update()
+}
+
+animate()
+
+console.log(player)
